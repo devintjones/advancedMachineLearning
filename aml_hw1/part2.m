782f001adf00e31f7f9e0d212e74e3e6c75d3fbf
devintjones <devin.thatcher.jones@gmail.com>
2015-02-02 23:29:19 -0500

    converted data format to BW image. working 2d embedding

--- aml_hw1/part2.m
+++ aml_hw1/part2.m
@@ -5,15 +5,17 @@
 function part2
 run('C:\Users\Devin\Documents\MATLAB\mve-05\setuppath.m');
 
-load('digits9.mat');
 
 %load digits9.mat; for i=1:155; plot(digits9(i,1:2:140),digits9(i,2:2:140),'.'); pause(0.4); end
-X = digits9;
+load('digits9.mat');
+
+X = convertDigits(digits9,0);
+
 [D, N] = size(X);
 disp(sprintf('%d points in %d dimensions:', N, D));
 
 % Calculate linear kernal. 
-A = calculateAffinityMatrix(X,3,0,4);
+A = calculateAffinityMatrix(X,1,0,0);
 
 % Derive distance matrix from kernal
 G = convertAffinityToDistance(A);
@@ -36,6 +38,40 @@ tol = 0.99;
 disp(sprintf('The size of Y is %d,%d',size(Y)));
 plotEmbedding(Y, neighbors, 'MVE embedding' ,35)
 
+%Converts the digits9 dataset into BW image format
+function [new_x] = convertDigits(digits9,movie)
+
+    X = digits9;
+
+    %Get dimensions of images. Its index begins at zero, so add one to comply
+    %with matlab indexing
+    max_dim = max(max(X)) + 1;
+
+    [rows,cols] = size(X);
+
+    % This will house all images of digits in its rows
+    new_x = zeros(rows,max_dim^2);
+
+    for i = 1:rows;
+        %This will populate with pixels from digit9
+        empty_mat = zeros(max_dim,max_dim);
+        for j=1:2:cols
+            y_val = max_dim - X(i,j+1);
+            x_val = X(i,j) +1;
+            empty_mat(y_val,x_val) = 1;
+        end
+        %disp(sprintf('Size of empty_mat: %d by %d',size(empty_mat)));
+        new_x(i,:) = reshape(empty_mat,1,max_dim^2);
+    end
+    
+    % Check format of the first frame
+    first_frame = reshape(new_x(1,:),max_dim,max_dim);
+    
+    % Watch movie of frames
+    if movie == 1
+        for i=1:rows; imshow(reshape(new_x(i,:),max_dim,max_dim)); pause(0.4); end;
+    end
+
 %This is from Blake Shaw's mvedriver.m
 %Devin added polynomial kernel
 function [A] = calculateAffinityMatrix(X, affType, sigma, d)
@@ -57,7 +93,7 @@ function [A] = calculateAffinityMatrix(X, affType, sigma, d)
         R3 = Z * ones(N, 1)';
         A  = exp((1/sigma) * R1 - (1/(2*sigma)) * R2 - (1/(2*sigma)) * R3);
     elseif affType == 3
-        disp(sprintf('Using Polynomical Kernel'));
+        disp(sprintf('Using Polynomial Kernel'));
         A = (X' * X)^d;
     end
     
